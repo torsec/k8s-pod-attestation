ccd9febb9cbba77c4d99aa7faadabf134d09a653
francesco <francesco.zaritto@gmail.com>
2025-03-03 14:18:09 +0100

    update on pkgs: model

---
+++ pkg/model/const.go
@@ -0,0 +1,10 @@
+package model
+
+const (
+	newStatus               = "NEW"
+	trustedStatus           = "TRUSTED"
+	untrustedStatus         = "UNTRUSTED"
+	unknownStatus           = "UNKNOWN"
+	deletedStatus           = "DELETED"
+	podAttestationNamespace = "attestation-system"
+)
