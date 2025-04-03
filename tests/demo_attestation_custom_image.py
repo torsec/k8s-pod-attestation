2d77c60c33c68387811abc4b8379824467411c54
francesco <francesco.zaritto@gmail.com>
2025-04-03 18:31:07 +0200

    fix: add b64 encode on test message

--- tests/demo_attestation_custom_image.py
+++ tests/demo_attestation_custom_image.py
@@ -96,7 +96,7 @@ create_tenant(tenant_name, public_key)
 signature = sign_message(message)
 
 # Verify the signature
-verify_signature(tenant_name, message, signature)
+verify_signature(tenant_name, base64.b64encode(message), signature)
 
 signature = sign_message(pod_name)
 
