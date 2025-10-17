28ab89fca9a48c3d0d432229e0518fd28a5052a8
francesco <francesco.zaritto@gmail.com>
2025-10-17 15:49:08 +0200

    fix: readded AgentConfig in model

--- pkg/model/types.go
+++ pkg/model/types.go
@@ -127,3 +127,12 @@ type AttestationEvidence struct {
 	Evidence  Evidence `json:"evidence"`
 	Signature string   `json:"signature,omitempty"`
 }
+
+type AgentConfig struct {
+	TPMPath                 string `json:"TPMPath"`
+	IMAMountPath            string `json:"IMAMountPath"`
+	IMAMeasurementLogPath   string `json:"IMAMeasurementLogPath"`
+	ImageName               string `json:"imageName"`
+	AgentPort               int32  `json:"agentPort"`
+	AgentNodePortAllocation int32  `json:"agentNodePortAllocation"`
+}
